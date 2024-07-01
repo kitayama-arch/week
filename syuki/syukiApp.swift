1cf2a6ac36d99475127999787c1b4fe642210ff6
ito541 <133589217+ito541@users.noreply.github.com>
2024-07-01 14:27:25 +0900

    Initial Commit

---
+++ syuki/syukiApp.swift
@@ -0,0 +1,17 @@
+//
+//  syukiApp.swift
+//  syuki
+//
+//  Created by Ta-MacbookAir on 2024/07/01.
+//
+
+import SwiftUI
+
+@main
+struct syukiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
