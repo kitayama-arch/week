d972b39e22d38e86616468fda70bf41156ed558c
ito541 <133589217+ito541@users.noreply.github.com>
2024-08-10 14:14:25 +0900

    detamanager

--- syuki/Service/DataManager.swift
+++ syuki/Service/DataManager.swift
@@ -28,5 +28,35 @@ class DataManager: ObservableObject {
         }
     }
     
-    
+    func createThoughtCard(content: String, date: Date, items: [String]) {
+        if let entity = coreDataManager.createThoughtCard(content: content, date: date, items: items) {
+            guard let content = entity.content,
+                  let date = entity.date,
+                  let items = entity.items else {
+                print("ThoughtCardの作成に失敗しました:データのアンラップに失敗")
+                return
+            }
+            
+            let newThoughtCard = ThoughtCard(content: content, date: date, items: items)
+            thoughtCards.append(newThoughtCard)
+        } else {
+            print("ThoughtCardの作成に失敗しました")
+        }
+    }
+    func updateThoughtCard(thoughtCard: ThoughtCard, newContent: String) {
+        guard let index = thoughtCards.firstIndex(where: { $0.id == thoughtCard.id}),
+              let entity = coreDataManager.readThoughtCards().first(where: { $0.id == thoughtCard.id}) else { return }
+        
+        coreDataManager.updateThoughtCard(thoughtCard: entity, newContent: newContent)
+        thoughtCards[index].content = newContent
+    }
+    func deleteThoughtCard(at offsets: IndexSet) {
+        offsets.forEach { Index in
+            let thoughtCard = thoughtCards[Index]
+            if let entity = coreDataManager.readThoughtCards().first(where: { $0.id == thoughtCard.id }) {
+                coreDataManager.deleteThoughtCard(thoughtCard: entity)
+            }
+        }
+        thoughtCards.remove(atOffsets: offsets)
+    }
 }
