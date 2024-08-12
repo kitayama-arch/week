c3933197806a52c12e1d27501673ad6b7c2ea42e
ito541 <133589217+ito541@users.noreply.github.com>
2024-08-12 18:28:11 +0900

    weeklymodel

--- syuki/Models/WeeklyRecord.swift
+++ syuki/Models/WeeklyRecord.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+struct WeeklyRecord: Identifiable {
+    let id: UUID
+    let startDate: Date
+    let endDate: Date
+    var thoughts: [ThoughtCard]
+    var reflection: String
+    var goal: String
+    var nextWeekGoal: String
+    
+    init(id: UUID, startDate: Date, endDate: Date, thoughts: [ThoughtCard], reflection: String, goal: String, nextWeekGoal: String) {
+        self.id = id
+        self.startDate = startDate
+        self.endDate = endDate
+        self.thoughts = thoughts
+        self.reflection = reflection
+        self.goal = goal
+        self.nextWeekGoal = nextWeekGoal
+    }
+}
