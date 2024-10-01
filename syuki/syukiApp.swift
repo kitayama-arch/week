abab3b3b0e6b287c796264171d5b81737cf435f6
ito541 <133589217+ito541@users.noreply.github.com>
2024-10-01 13:18:15 +0900

    admobとfirebase同時に

--- syuki/syukiApp.swift
+++ syuki/syukiApp.swift
@@ -7,19 +7,22 @@
 
 import SwiftUI
 import FirebaseCore
+import GoogleMobileAds
 
-class AppDelegate: NSObject, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  
   func application(_ application: UIApplication,
-                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-    FirebaseApp.configure()
-
-    return true
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+      FirebaseApp.configure()
+      GADMobileAds.sharedInstance().start(completionHandler: nil)
+      return true
   }
 }
 
 @main
 struct syukiApp: App {
-    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
             HomeView()
