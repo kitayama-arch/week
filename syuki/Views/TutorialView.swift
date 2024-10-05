f8119ff8f0581b76024fcb9b01322fc91b5652a4
ito541 <133589217+ito541@users.noreply.github.com>
2024-10-05 17:41:16 +0900

    チュートリアル文言編集

--- syuki/Views/TutorialView.swift
+++ syuki/Views/TutorialView.swift
@@ -12,11 +12,11 @@ struct TutorialView: View {
     @Environment(\.dismiss) private var dismiss
     
     let tutorialPages = [
-        TutorialPage(image: "tutorial1", title: "ようこそ！Syukiの世界へ", description: "Syukiは、1週間を意識的に過ごすための、あなたのパーソナルアシスタントです。"),
+        TutorialPage(image: "tutorial1", title: "ようこそ！Syukiの世界へ", description: "Syukiを使えば、「気づいたら一週間経ってた」がなくなります。"),
         TutorialPage(image: "tutorial2", title: "流れる日々を記録する", description: "毎日の出来事、アイデア、感情など、どんなことでも自由に書き留めましょう。思いついたときに、気軽に記録してみましょう。"),
         TutorialPage(image: "tutorial3", title: "少しでも前に進む", description: "週の目標を設定し、一歩ずつ前進しましょう。小さな進歩が大きな変化を生み出します。"),
-        TutorialPage(image: "tutorial4", title: "自分と比べる", description: "毎週日曜日は、Syukiで一週間を振り返る日。\n良かったこと、次週への改善策などをゆっくり整理し、自分の成長を確認しましょう。"),
-        TutorialPage(image: "tutorial5", title: "いい生活はいい一週間から", description: "もう「気づいたら一週間経ってた」なんて言わせません。Syukiと共に、意識的な1週間を過ごしましょう。")
+        TutorialPage(image: "tutorial4", title: "過去の自分と比べる", description: "毎週日曜日は、Syukiで一週間を振り返る日。\n良かったこと、次週への改善策などをゆっくり整理し、自分の成長を確認しましょう。"),
+        TutorialPage(image: "tutorial5", title: "いい生活はいい一週間から", description: "Syukiと共に、意識的な1週間を過ごしましょう。")
     ]
     
     var body: some View {
@@ -53,12 +53,12 @@ struct TutorialView: View {
                     Text(currentPage < tutorialPages.count - 1 ? "次へ" : "始める")
                         .foregroundColor(.white)
                         .padding()
-                        .frame(maxWidth: .infinity)
+                        .padding(.horizontal, 20)
                         .background(Color.accentColor)
-                        .cornerRadius(10)
+                        .clipShape(Capsule()) // ここを変更
                 }
                 .padding(.horizontal)
-                .padding(.bottom, 40)
+                .padding(.bottom, 20)
             }
         }
     }
@@ -79,7 +79,7 @@ struct TutorialPageView: View {
             Image(page.image)
                 .resizable()
                 .scaledToFit()
-                .frame(height: 250)
+                .frame(height: 500)
             
             Text(page.title)
                 .font(.title2)
