0a9ab7c42e6387ffc6e83e19d9955ced5332a011
ito541 <133589217+ito541@users.noreply.github.com>
2024-10-06 23:07:21 +0900

    チュートリアルページのnextボタンにグラデーションとシャドウ

--- syuki/Views/TutorialView.swift
+++ syuki/Views/TutorialView.swift
@@ -12,11 +12,11 @@ struct TutorialView: View {
     @Environment(\.dismiss) private var dismiss
     
     let tutorialPages = [
-        TutorialPage(image: "tutorial1", title: "ようこそ！Syukiの世界へ", description: "Syukiを使えば、「気づいたら一週間経ってた」がなくなります。"),
+        TutorialPage(image: "tutorial1", title: "ようこそ！weekの世界へ", description: "weekを使えば、「気づいたら一週間経ってた」がなくなります。"),
         TutorialPage(image: "tutorial2", title: "流れる日々を記録する", description: "毎日の出来事、アイデア、感情など、どんなことでも自由に書き留めましょう。思いついたときに、気軽に記録してみましょう。"),
         TutorialPage(image: "tutorial3", title: "少しでも前に進む", description: "週の目標を設定し、一歩ずつ前進しましょう。小さな進歩が大きな変化を生み出します。"),
         TutorialPage(image: "tutorial4", title: "過去の自分と比べる", description: "毎週日曜日は、Syukiで一週間を振り返る日。\n良かったこと、次週への改善策などをゆっくり整理し、自分の成長を確認しましょう。"),
-        TutorialPage(image: "tutorial5", title: "いい生活はいい一週間から", description: "Syukiと共に、意識的な1週間を過ごしましょう。")
+        TutorialPage(image: "tutorial5", title: "いい生活は、いい一週間から。", description: "Syukiと共に、意識的な1週間を過ごしましょう。")
     ]
     
     var body: some View {
@@ -32,11 +32,18 @@ struct TutorialView: View {
                 }
                 .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
                 
-                HStack {
+                HStack(spacing: 8) {
                     ForEach(0..<tutorialPages.count, id: \.self) { index in
-                        Circle()
-                            .fill(currentPage == index ? Color.primary : Color.secondary)
-                            .frame(width: 8, height: 8)
+                        if currentPage == index {
+                            Capsule()
+                                .fill(Color.accentColor)
+                                .frame(width: 20, height: 8)
+                                .transition(.scale)
+                        } else {
+                            Circle()
+                                .fill(Color.secondary)
+                                .frame(width: 8, height: 8)
+                        }
                     }
                 }
                 .padding(.bottom, 20)
@@ -51,11 +58,21 @@ struct TutorialView: View {
                     }
                 }) {
                     Text(currentPage < tutorialPages.count - 1 ? "次へ" : "始める")
-                        .foregroundColor(.white)
-                        .padding()
+                        .font(.headline)
+                        .foregroundColor(.white.opacity(0.95))
+                        .padding(.vertical, 12)
                         .padding(.horizontal, 20)
-                        .background(Color.accentColor)
-                        .clipShape(Capsule())
+                        .background(
+                            LinearGradient(
+                                gradient: Gradient(colors: [
+                                    Color.accentColor.opacity(0.8),Color.accentColor
+                                ]),
+                                startPoint: .top,
+                                endPoint: .bottom
+                            )
+                        )
+                        .clipShape(.capsule)
+                        .shadow(color: .accent.opacity(0.5), radius: 10, x: 0.0, y: 4)
                 }
                 .padding(.horizontal)
                 .padding(.bottom, 20)
