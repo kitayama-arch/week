d2ef9078471280d5e8788d700707d1982656b46b
ito541 <133589217+ito541@users.noreply.github.com>
2024-10-07 03:16:32 +0900

    プライバシーポリシーURL変更

--- syuki/Views/HomeView.swift
+++ syuki/Views/HomeView.swift
@@ -236,9 +236,9 @@ struct HomeView: View {
                             UIApplication.shared.closeKeyboard()
                             print("カード間の空白部分がタップされました")
                         }
-                        Spacer()
-                        AdMobBannerView()
-                            .frame(width: 320, height: 50)
+//                        Spacer()
+//                        AdMobBannerView()
+//                            .frame(width: 320, height: 50)
                     }
                 } else {
                     // currentWeeklyRecord が nil の場合：振り返り未完了の状態を表示
