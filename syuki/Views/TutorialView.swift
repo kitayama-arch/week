05ad721fe4f00962f746fefe3a9ca262e5138c59
ito541 <133589217+ito541@users.noreply.github.com>
2024-10-08 10:26:36 +0900

    チュートリアル画像追加

--- syuki/Views/TutorialView.swift
+++ syuki/Views/TutorialView.swift
@@ -12,8 +12,8 @@ struct TutorialView: View {
     @Environment(\.dismiss) private var dismiss
     
     let tutorialPages = [
-        TutorialPage(image: "tutorial1", title: "ようこそ！weekの世界へ", description: "weekを使えば、\n「気づいたら一週間経ってた」がなくなります。"),
-        TutorialPage(image: "tutorial2", title: "流れる日々を記録する", description: "出来事、アイデア、感情、\nどんなことでも自由に書き留めましょう。\n日記のように毎日記録する必要はありません"),
+        TutorialPage(image: "tutorial1", title: "weekへようこそ", description: "weekを使えば、\n「気づいたら一週間経ってた」がなくなります。"),
+        TutorialPage(image: "tutorial2", title: "流れる日々を記録する", description: "出来事、アイデア、感情、\nどんなことでも自由に書き留めましょう。\n日記のように毎日記録する必要はありません。"),
         TutorialPage(image: "tutorial3", title: "少しでも前に進む", description: "週の目標を設定し、一歩ずつ前進しましょう。\n小さな進歩が大きな変化を生み出します。"),
         TutorialPage(image: "tutorial4", title: "過去の自分と比べる", description: "毎週日曜日は、weekで一週間を振り返る日。\n良かったこと、次週への改善策などを\nゆっくり整理し、自分の成長を確認しましょう。"),
         TutorialPage(image: "tutorial5", title: "いい生活は、いい一週間から。", description: "")
@@ -38,7 +38,6 @@ struct TutorialView: View {
                             Capsule()
                                 .fill(Color.accentColor)
                                 .frame(width: 20, height: 8)
-                                .transition(.scale)
                         } else {
                             Circle()
                                 .fill(Color.secondary)
@@ -96,7 +95,8 @@ struct TutorialPageView: View {
             Image(page.image)
                 .resizable()
                 .scaledToFit()
-                .frame(height: 500)
+                .frame(height: 400)
+                .padding()
             
             Text(page.title)
                 .font(.title2)
